9b37cef41554de68c1e077ffa27d1bb3c96be2ba
Samuel Colbran <contact@samuco.net>
2015-01-28 22:11:09 +1000

    updated queues

--- core/queue.js
+++ core/queue.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Samuel Colbran <contact@samuco.net>
+ * Copyright (c) 2015, Samuel Colbran <contact@samuco.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -24,269 +24,198 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-var crypto 	 = require('crypto');
-var database   = require('./database');
-var experiment = require('./experiment');
-var defines    = require('./defines');
-var calendar    = require('./calendar');
+var Heap = require('heap');
+var db = require("./database");
 
-var queue_module = module.exports;
-queue_module._ram_queue = undefined;
-queue_module._validation_queue = [];
+module.exports.queueState = Object.freeze({
+    EMPTY: 1,
+    READY: 2,
+    RESTRICTED: 3
+});
 
-queue_module.addVElement = function(vobject)
-{
-    queue_module._validation_queue.push(vobject);
-}
-queue_module.numberOfVElements = function()
-{
-    return queue_module._validation_queue.length;
-}
-queue_module.nextVElement = function()
-{
-    if (queue_module.numberOfVElements())
-    {
-        var vobject = queue_module._validation_queue[0];
-        queue_module._validation_queue.shift(0);
-        return vobject;
-    }
-    return undefined;
-}
+module.exports.createQueue = function() {
+    return new function() {
+        var queue = this;
+        queue._database = undefined;
+        queue._heap = undefined;
 
-/**
- * Return the current queue length
- * @returns {Number} - queue length
- */
-queue_module.queueLength = function()
-{
-    return database.getKeys('experiment_queue').length;
-};
+        var heap_compare = function (a, b) {
+            return parseInt(a.experimentID) - parseInt(b.experimentID);
+        };
 
-/**
- * Return the estimated time until all experiments are completed
- * @returns {number} - estimated time
- */
-queue_module.estimatedWait = function()
-{
-    //TODO: Handle queue priority
+        /**
+         * Add an experiment to the queue
+         * @param experiment
+         */
+        queue.add = function(experiment) {
+            Heap.push(queue._heap, experiment, heap_compare);
+            queue.save(function() {
 
-    // Go through each experiment in the queue and calculate the total wait time
-    var time = 0;
-    for (var i = 0; i < queue_module._ram_queue.length; i++) {
-        var vReport = queue_module._ram_queue[i].vReport;
-        if (typeof vReport !== 'undefined') {
-            time += vReport.estRuntime;
+            });
         }
-    }
 
-    return time;
-};
+        /**
+         * Sets the memory queue
+         * @param cache
+         * @private
+         */
+        queue._load = function (heap) {
+            queue._heap = (typeof heap !== 'undefined') ? heap : [];
+        };
 
-/**
- * Returns the effective length javascript dictionary
- * @returns {{effectiveQueueLength: string, estWait: string}}
- */
-queue_module.getEffectiveQueueLength = function()
-{
-    return {effectiveQueueLength: String(queue_module.queueLength()), estWait: String(queue_module.estimatedWait())};
-};
+        /**
+         * Save the queue to the database
+         * @param callback
+         */
+        queue.save = function (callback) {
+            db.setValueForKey("queue", queue._database, queue._heap, function () {
+                callback();
+            })
+        };
 
-/**
- * Returns the next experiment id
- * @returns int - next id
- */
-queue_module.nextExperiment = function()
-{
-    var next_experiment = database.valueForKey("settings", "next_experiment", undefined);
-    next_experiment = (typeof next_experiment !== 'undefined') ? next_experiment : 0;
-    return next_experiment;
-};
+        /**
+         * Loads the queue from the database
+         */
+        queue.load = function (database, callback) {
+            queue._database = database;
+            db.valueForKey("queue", queue._database, function (err, cache) {
+                queue._load(cache);
+                callback();
+            });
+        };
 
-/**
- * Increments the next experiment id
- * @returns int - next id
- */
-queue_module.incrementExperimentId = function()
-{
-    var next_experiment = queue_module.nextExperiment();
-    next_experiment++;
-    database.setValueForKey("settings", "next_experiment", next_experiment, undefined);
-    return next_experiment-1;
-};
-
-/**
- * Loads the ram queue from the database
- * @private
- */
-queue_module._loadQueue = function()
-{
-    queue_module._ram_queue = database.valueForKey("queue", "cache", undefined);
-    queue_module._ram_queue = (typeof queue_module._ram_queue !== 'undefined') ? queue_module._ram_queue : [];
-};
-
-/**
- * Saves the ram queue to the database
- * @private
- */
-queue_module._saveQueue = function()
-{
-    queue_module._ram_queue = (typeof queue_module._ram_queue !== 'undefined') ? queue_module._ram_queue : [];
-    database.setValueForKey("queue", "cache", queue_module._ram_queue, undefined);
-};
-
-/**
- * Adds an experiment to the queue
- * @param experiment
- */
-queue_module.add = function(experiment)
-{
-    queue_module._ram_queue.push(experiment);
-    queue_module._saveQueue();
-};
-
-/**
- * Removes the experiment at the top of the queue
- */
-queue_module.removeExperiment = function(experimentID)
-{
-    var i;
-    for (i=0; i < queue_module._ram_queue.length; i++)
-    {
-        var queued_experiment = queue_module._ram_queue[i];
-        if (parseInt(queued_experiment['experimentID']) == parseInt(experimentID))
-        {
-            defines.prettyLine("experiment queue", "deleted " + experimentID);
-            queue_module._ram_queue.splice(i,1);
-            queue_module._saveQueue();
-            return true;
-        }
-    }
-    defines.prettyLine("experiment queue", "missing" + experimentID);
-    return false;
-};
-
-/**
- * Removes the experiment at the top of the queue
- */
-/*
- queue_module.next = function()
- {
- queue_module._ram_queue.shift(0);
- queue_module._saveQueue();
- };
- */
-
-queue_module.containsExperiment = function(experimentID)
-{
-    var i;
-    for (i=0; i < queue_module._ram_queue.length; i++)
-    {
-        var queued_experiment = queue_module._ram_queue[i];
-        if (queued_experiment['experimentID'] == experimentID)
-            return true;
-    }
-    return false;
-}
-
-var refreshTimer = null;
-
-/**
- * Checks the queue state. Starts the next experiment if possible.
- */
-queue_module.pollQueue = function()
-{
-    if (refreshTimer)
-    {
-        clearTimeout(refreshTimer);
-        refreshTimer = null;
-    }
-
-    if (queue_module._ram_queue.length > 0)
-    {
-        //Find an experiment with suitable access privileges
-        defines.verbose("QUEUE: Finding next experiment...");
-        var next_experiment = undefined;
-        var i;
-        var selected_index;
-        for (i=0; i < queue_module._ram_queue.length; i++)
-        {
-            var queued_experiment = queue_module._ram_queue[i];
-            if (typeof queued_experiment['guid'] !== 'undefined') {
-                var brokerName = broker.findBroker(queued_experiment['guid']).getName();
+        /**
+         * Checks whether an experiment is restricted
+         * @param experiment
+         * @returns {boolean}
+         */
+        queue.isRestricted = function (experiment) {
+            var guid = experiment['guid'];
+            if (typeof guid !== 'undefined') {
+                var broker_name = broker.findBroker(guid).getName();
                 var runtime = queued_experiment['vReport']['estRuntime'];
-                if (calendar.hasAccess(brokerName, runtime))
-                {
-                    queue_module._ram_queue[i].queueStatus = -1;
-                    if (next_experiment == undefined)
-                    {
-                        selected_index = i;
-                        next_experiment = queued_experiment;
-                    }
-                    //break;
+                if (!calendar.hasAccess(broker_name, runtime)) {
+                    return true;
                 }
-                else
-                {
-                    queue_module._ram_queue[i].queueStatus = defines.kRestricted;
+            }
+            return false;
+        };
+
+        /**
+         *
+         * @returns {*}
+         */
+        queue.pop = function() {
+            return Heap.pop(queue._heap, heap_compare);
+        }
+
+        /**
+         * Returns the next experiment to run
+         * @returns {*}
+         */
+        queue.getNext = function () {
+            if (queue._heap.length == 0) {
+                return undefined;
+            }
+
+            // Is the top experiment suitable?
+            var top = queue._heap[0];
+            if (!queue.isRestricted(top)) {
+                return top;
+            }
+
+            // Rebuild the heap order and try again
+            Heap.heapify(queue._heap, heap_compare);
+            var top = queue._heap[0];
+            if (!queue.isRestricted(top)) {
+                return top;
+            }
+
+            // The queue is restricted
+            return undefined;
+        };
+
+        /**
+         * Returns the state of the heap
+         * @returns {number}
+         */
+        queue.getState = function () {
+            if (queue._heap.length > 0) {
+                if (typeof queue.getNext() !== 'undefined') {
+                    return module.exports.queueState.READY;
+                } else {
+                    return module.exports.queueState.RESTRICTED;
                 }
             } else {
-                // This experiment was not submitted by a broker
-                queue_module._ram_queue[i].queueStatus = -1;
-                if (next_experiment == undefined)
-                {
-                    selected_index = i;
-                    next_experiment = queued_experiment;
+                return module.exports.queueState.EMPTY;
+            }
+        };
+
+        // Copy event properties
+        var events = require("events");
+        queue.__proto__ = events.EventEmitter.prototype;
+
+        // 1.0.3 functions
+        queue.containsExperiment = function (experimentID) {
+            var ram_queue = queue._heap;
+            var i;
+            for (i = 0; i < ram_queue.length; i++) {
+                var queued_experiment = ram_queue[i];
+                if (queued_experiment['experimentID'] == experimentID) {
+                    return true;
                 }
             }
-        }
-        queue_module._saveQueue();
+            return false;
+        };
 
-        if (typeof next_experiment !== 'undefined')
-        {
-            var experimentId = next_experiment['experimentID'];
+        /**
+         * Return the current queue length
+         * @returns {Number} - queue length
+         */
+        queue.queueLength = function () {
+            return queue._heap.length;
+        };
 
-            //Is there an experimentment currently running?
-            var experiment_status = experiment.getStatusCode();
-            if (experiment_status == defines.idle_status)
-            {
-                defines.verbose("QUEUE: Starting experiment " + experimentId);
-                defines.prettyLine("experiment queue", "starting " + experimentId);
-                queue_module._ram_queue[selected_index].queueStatus = defines.kRunning;
-                queue_module._saveQueue();
-                experiment.startExperiment(next_experiment);
+        /**
+         * Return the estimated time until all experiments are completed
+         * @returns {number} - estimated time
+         */
+        queue.estimatedWait = function () {
+            //TODO: Handle queue priority
+
+            // Go through each experiment in the queue and calculate the total wait time
+            var ram_queue = queue._heap;
+            var time = 0;
+            for (var i = 0; i < ram_queue.length; i++) {
+                var vReport = ram_queue[i].vReport;
+                if (typeof vReport !== 'undefined') {
+                    time += vReport.estRuntime;
+                }
             }
-            else
-            {
-                defines.prettyLine("experiment queue", "busy");
-                defines.verbose("QUEUE: Busy");
-            }
-        }
-        else
-        {
-            defines.prettyLine("experiment queue", queue_module._ram_queue.length + " restricted");
-            defines.verbose("QUEUE: Restricted. There are " + queue_module._ram_queue.length + " experiments waiting to begin execution.");
+            return time;
+        };
 
-            //Refresh the queue in a minute
-            refreshTimer = setTimeout(queue_module.pollQueue, 60*1000);
-        }
-    }
-    else
-    {
-        defines.prettyLine("experiment queue", "empty");
-        defines.verbose("QUEUE: Empty");
+        /**
+         * Returns the next experiment id
+         * @returns int - next id
+         */
+        queue.nextExperiment = function () {
+            var next_experiment = db.valueForKey("settings", "next_experiment", undefined);
+            next_experiment = (typeof next_experiment !== 'undefined') ? next_experiment : 0;
+            return next_experiment;
+        };
+
+        /**
+         * Increments the next experiment id
+         * @returns int - next id
+         */
+        queue.incrementExperimentId = function () {
+            var next_experiment = queue.nextExperiment();
+            next_experiment++;
+            db.setValueForKey("settings", "next_experiment", next_experiment, undefined);
+            return next_experiment - 1;
+        };
     }
 };
 
-/**
- * Loads the queue and starts executing experiments
- * @param callback
- */
-queue_module.startQueue = function(callback)
-{
-    queue_module._loadQueue();
-    defines.prettyLine("queue", defines.loaded);
-    defines.prettyConsole("\r\nStarting experiment queue\r\n");
 
-    //Immediately poll the queue
-    queue_module.pollQueue();
-    callback();
-};
